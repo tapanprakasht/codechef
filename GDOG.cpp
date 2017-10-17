fedf484d1b741da65caf5e22850bbf462f1da4ef
Tapan Prakash <tapanprakasht@gmail.com>
2017-10-17 21:29:00 +0530

    Solution for GDOG

---
+++ GDOG.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[]) {
+  long long t,n,k;
+  cin>>t;
+  while(t--){
+    cin>>n>>k;
+    cout<<n%k<<endl;
+  }
+  return 0;
+}
